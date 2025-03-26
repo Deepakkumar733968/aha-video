0847beac80cc575e426798f293d635d27cf0de67
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-03-26 07:02:44 +0530

    changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -13,9 +13,10 @@ function App() {
         image={
           "https://image-resizer-cloud-api.akamaized.net/image/49B283E2-29FA-46A9-A2DA-8822C9EDBE22/0-2x3.jpg?width=305&updatedTime=2025-03-25T06:18:28Z&dt=Web"
         }
-        value={"Mr Housekeeping"}s
+        value={"Mr Housekeeping"}
+        Premium={false}
       /> */}
-      <SeeAll heading={"New Releases"} seeAll={"See all"} className={""} />
+      {/* <SeeAll heading={"New Releases"} seeAll={"See all"} className={""} /> */}
     </main>
   );
 }
--- my-app/src/components/MovieCard/index.tsx
+++ my-app/src/components/MovieCard/index.tsx
@@ -6,6 +6,7 @@ interface ICardData {
   imageClass?: string;
   image: string;
   value: string;
+  Premium?: boolean;
 }
 
 export const MovieCard = ({
@@ -14,18 +15,21 @@ export const MovieCard = ({
   imageClass = "",
   image,
   value,
+  Premium = true,
 }: ICardData) => {
   return (
     <div className={`${mainDivClass} img-card-main`}>
-      <span className="add-premium-pos">
-        <img
-          src="https://www.aha.video/tag-icon.4a97c58d9b826d7d.svg"
-          alt="right-icons"
-          className="right-icons-size"
-        />
-        <span className="pos-crd-txt">Premium</span>
-      </span>
-      <div className={`${imageDivClass} img-main-div`}>
+      {Premium && (
+        <span className="add-premium-pos">
+          <img
+            src="https://www.aha.video/tag-icon.4a97c58d9b826d7d.svg"
+            alt="right-icons"
+            className="right-icons-size"
+          />
+          <span className="pos-crd-txt">Premium</span>
+        </span>
+      )}
+      <div className={`${imageDivClass} img-main-div cursor-pointer`}>
         <img
           src={image}
           alt="movie-card-image"
