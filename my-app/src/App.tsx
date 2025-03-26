2bc23b8a95303b83ab0f750a19c1d5f0c6d3f214
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-03-26 17:25:45 +0530

    changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,20 +8,35 @@ import { MOVIES_LIST } from "./utils/Home/home";
 function App() {
   return (
     <main>
-      {/* <HeroSlider /> */}
       {/* <ChatWithExpert /> */}
-      {/* <MovieCard
-        imageDivClass={"img-dic-cls"}
-        imageClass={"img-cls"}
-        image={
-          "https://image-resizer-cloud-api.akamaized.net/image/49B283E2-29FA-46A9-A2DA-8822C9EDBE22/0-2x3.jpg?width=305&updatedTime=2025-03-25T06:18:28Z&dt=Web"
-        }
-        value={"Mr Housekeeping"}
-        Premium={false}
-      /> */}
-      {/* <SeeAll heading={"New Releases"} seeAll={"See all"} className={""} /> */}
       <MoviesList data={MOVIES_LIST} />
-      <Footer />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
+      <MoviesList data={MOVIES_LIST} />
     </main>
   );
 }
--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import NavBar from "./layouts/NavBar/NavBar.tsx";
+import Footer from "./layouts/Footer/Footer.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <NavBar /> */}
+    <NavBar />
     <App />
+    <Footer />
   </StrictMode>
 );
