1c119ea05824022fcc0d86f18067af13b4c0aa6e
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-03-26 05:57:00 +0530

    add new img-crd-component

--- my-app/src/components/MovieCard/index.tsx
+++ my-app/src/components/MovieCard/index.tsx
@@ -1,25 +1,35 @@
 import "./style.css";
+
 interface ICardData {
   mainDivClass?: string;
-  imageDivClass: string;
+  imageDivClass?: string;
   imageClass?: string;
   image: string;
   value: string;
 }
+
 export const MovieCard = ({
-  mainDivClass,
-  imageDivClass,
-  imageClass,
+  mainDivClass = "",
+  imageDivClass = "",
+  imageClass = "",
   image,
   value,
 }: ICardData) => {
   return (
-    <div className={mainDivClass}>
-      <div className={`${imageDivClass}`}>
+    <div className={`${mainDivClass} img-card-main`}>
+      <span className="add-premium-pos">
+        <img
+          src="https://www.aha.video/tag-icon.4a97c58d9b826d7d.svg"
+          alt="right-icons"
+          className="right-icons-size"
+        />
+        <span className="pos-crd-txt">Premium</span>
+      </span>
+      <div className={`${imageDivClass} img-main-div`}>
         <img
           src={image}
           alt="movie-card-image"
-          className={`${imageClass}img-size`}
+          className={`${imageClass} img-size`}
         />
       </div>
       <p className="img-crd-des">{value}</p>
