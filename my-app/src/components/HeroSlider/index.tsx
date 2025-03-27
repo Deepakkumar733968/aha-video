1f9ccc18f788feeecb7acdab9b0a944f7cf8ca3f
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-03-27 19:02:43 +0530

    changes

--- my-app/src/components/HeroSlider/index.tsx
+++ my-app/src/components/HeroSlider/index.tsx
@@ -1,8 +1,9 @@
 import "./style.css";
-
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md";
 export const HeroSlider = () => {
   return (
-    <section className="hero-main">
+    <section className="hero-main cursor-pointer">
       <div className="left-pos-main">
         <div>
           <h1 className="pos-hed">Ring Ring</h1>
@@ -18,12 +19,35 @@ export const HeroSlider = () => {
       {/* <div className="bg-left-div-main"></div> */}
       <div className="l-a-r-main">
         <div className="bg-left-div-main"></div>
-        <div className="m-left-main">
-          <h1>left</h1>
+        <div className="m-left-main">{/* <h1>left</h1> */}</div>
+        <div className="m-right-main">{/* <h1>right</h1> */}</div>
+      </div>
+      <div className="all-btn-img-ali-main">
+        <MdKeyboardArrowLeft size={26} />
+        <div className="last-left-carousel-main">
+          <div>
+            <img
+              src="https://image-resizer-cloud-api.akamaized.net/image/F2E3201D-E2C8-4BF0-927C-52DF484F8CBE/0-16x9.jpg?width=160&updatedTime=2025-03-27T09:21:25Z&dt=Web"
+              alt="small-carousel-img"
+              className="left-carousel-img"
+            />
+          </div>
+          <div>
+            <img
+              src="https://image-resizer-cloud-api.akamaized.net/image/F2E3201D-E2C8-4BF0-927C-52DF484F8CBE/0-16x9.jpg?width=160&updatedTime=2025-03-27T09:21:25Z&dt=Web"
+              alt="small-carousel-img"
+              className="left-carousel-img"
+            />
+          </div>
+          <div>
+            <img
+              src="https://image-resizer-cloud-api.akamaized.net/image/F2E3201D-E2C8-4BF0-927C-52DF484F8CBE/0-16x9.jpg?width=160&updatedTime=2025-03-27T09:21:25Z&dt=Web"
+              alt="small-carousel-img"
+              className="left-carousel-img"
+            />
+          </div>
         </div>
-        <div className="m-right-main">
-          <h1>right</h1>
-        </div>
+        <MdKeyboardArrowRight size={26} />
       </div>
     </section>
   );
