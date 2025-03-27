e4a65d06cb5ca842ab186f6e082221b8103928c4
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-03-27 12:21:48 +0530

    changes

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -7,7 +7,7 @@ import Footer from "./layouts/Footer/Footer.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <NavBar /> */}
+    <NavBar />
     <App />
     <Footer />
   </StrictMode>
