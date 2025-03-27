e48b11ea8153b538060785ee5b5a5d295cd7a3e1
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-03-27 05:32:51 +0530

    changes

--- my-app/src/components/ChatWithExpert/index.tsx
+++ my-app/src/components/ChatWithExpert/index.tsx
@@ -1,8 +1,28 @@
 import "./style.css";
 import { IoChatbubbleEllipses } from "react-icons/io5";
+import { useEffect, useState } from "react";
+
 export const ChatWithExpert = () => {
+  const [isAtPageEnd, setIsAtPageEnd] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.innerHeight + window.scrollY;
+      const pageHeight = document.documentElement.scrollHeight;
+      setIsAtPageEnd(scrollPosition >= pageHeight - 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <button className="chat-other-main cursor-pointer">
+    <button
+      className="chat-other-main cursor-pointer"
+      style={{
+        transform: isAtPageEnd ? "translateY(-78px)" : "translateY(0px)",
+      }}
+    >
       <div className="cht-img-main">
         <IoChatbubbleEllipses size={20} className="chat-icon" />
       </div>
