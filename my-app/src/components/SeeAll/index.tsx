7b4365f8facdd5da04e4c60d24b814871b71eaed
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-04-01 05:24:35 +0530

    changes

--- my-app/src/components/SeeAll/index.tsx
+++ my-app/src/components/SeeAll/index.tsx
@@ -3,12 +3,14 @@ interface ISeeAll {
   heading: string;
   seeAll: string;
   className: string;
+  showSeeAll: boolean;
 }
-export const SeeAll = ({ heading, seeAll, className }: ISeeAll) => {
+
+export const SeeAll = ({ heading, seeAll, className, showSeeAll }: ISeeAll) => {
   return (
     <div className={`${className} see-all-main`}>
       <h2 className="see-all-left-header">{heading}</h2>
-      <h2 className="see-all-right-header">{seeAll}</h2>
+      {showSeeAll && <h2 className="see-all-right-header">{seeAll}</h2>}
     </div>
   );
 };
--- my-app/src/utils/HeroCarousel/heroCarousel.ts
+++ my-app/src/utils/HeroCarousel/heroCarousel.ts
@@ -1,4 +1,5 @@
 export interface IHeroCarousel {
+  id: number;
   heading: string;
   subHeading: string;
   description: string;
@@ -8,6 +9,7 @@ export interface IHeroCarousel {
 }
 export const HERO_CAROUSEL: IHeroCarousel[] = [
   {
+    id: 1,
     heading: "Ring Ring",
     subHeading: "2025 • 1h 59m • Psychological Thriller",
     description:
@@ -20,6 +22,7 @@ export const HERO_CAROUSEL: IHeroCarousel[] = [
       "https://media.geeksforgeeks.org/wp-content/uploads/20190616234019/Canvas.move_.mp4",
   },
   {
+    id: 2,
     heading: "Operation Raavan",
     subHeading: "2024 • 2h 19m • Psychological Thriller",
     description:
@@ -32,6 +35,7 @@ export const HERO_CAROUSEL: IHeroCarousel[] = [
       "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
   },
   {
+    id: 3,
     heading: "Vera Maari Trip",
     subHeading: "2025 • Comedy • Drama",
     description:
@@ -41,5 +45,18 @@ export const HERO_CAROUSEL: IHeroCarousel[] = [
     smallHeroImage:
       "https://image-resizer-cloud-api.akamaized.net/image/9E7C2C6B-57D7-4BCF-92B9-1C278F6F4791/0-16x9.jpg?width=160&updatedTime=2025-03-25T06:02:24Z&dt=Web",
     heroVideo: "https://www.w3schools.com/html/mov_bbb.mp4",
+  },
+  {
+    id: 4,
+    heading: "Operation Raavan",
+    subHeading: "2024 • 2h 19m • Psychological Thriller",
+    description:
+      "In Operation Raavan, Anand Sriram, a multimillionaire's son, wants to join his own media channel as an employee, where he is employed to work under his longtime friend Amani. A serial killer kidnaps Amani and challenges both Anand Sriram and the police department. Did the police find the serial killer? will Anand Sriram rescue Amani?",
+    heroImage:
+      "https://image-resizer-cloud-api.akamaized.net/image/8B816CD0-3B40-4B8A-B7F9-49F45CB7C1C9/0-16x9.jpg?width=1250&updatedTime=2025-02-28T09:01:47Z&dt=Web",
+    smallHeroImage:
+      "https://image-resizer-cloud-api.akamaized.net/image/8B816CD0-3B40-4B8A-B7F9-49F45CB7C1C9/0-16x9.jpg?width=160&updatedTime=2025-02-28T09:01:47Z&dt=Web",
+    heroVideo:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
   },
 ];
