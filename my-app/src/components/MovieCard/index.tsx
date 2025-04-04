a2a8be5607a7c94c0a90c4fe37090aa49a83e3e5
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-04-04 17:38:35 +0530

    changes

--- my-app/src/components/MovieCard/index.tsx
+++ my-app/src/components/MovieCard/index.tsx
@@ -41,7 +41,7 @@ export const MovieCard = ({
       </div>
       <p className={`img-crd-des ${cardTitleClass}`}>{movie.lon[0].n}</p>
       {/* this is the A box in movies */}
-      <p>movie</p>
+      {/* <p>movie</p> */}
     </div>
   );
 };
