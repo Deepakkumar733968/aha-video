93dfcb4ea73e68f5ab001f633351ddcd78aef6a8
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-04-05 19:45:45 +0530

    changes

--- my-app/src/pages/Movies/page.tsx
+++ my-app/src/pages/Movies/page.tsx
@@ -1,29 +1,31 @@
+import InfiniteScroll from "react-infinite-scroll-component";
+import { HeroSlider } from "../../components/HeroSlider";
+import { MoviesList } from "../../components/MoviesList";
+import { useLandingScreenData } from "../../hooks/useLandingScreenData";
 import "./style.css";
 const Movies = () => {
-  // const { data, loading, error } = useLandingScreenData();
-  // if (loading) {
-  //   return <p>Loading...</p>;
-  // }
+  const { data, loading, error, fetchMore, hasMore } = useLandingScreenData();
 
-  // if (error) {
-  //   return <p>Error: {error}</p>;
-  // }
-
-  // if (!data) {
-  //   return <p>No data available</p>;
-  // }
-
-  // function containerData(containerNum: number) {
-  //   return data.t[1].c[containerNum];
-  // }
+  if (loading && data.length === 0) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
     <section>
-      {/* <HeroSlider heroCarousel={containerData(0)} />
-      <MoviesList movies={containerData(1)} />
-      <MoviesList movies={containerData(2)} />
-      <MoviesList movies={containerData(3)} />
-      <MoviesList movies={containerData(4)} /> */}
+      {data[0] && <HeroSlider heroCarousel={data[0]} />}
+
+      <InfiniteScroll
+        dataLength={data.length}
+        next={fetchMore}
+        hasMore={hasMore}
+        loader={<p style={{ display: "none" }}>Loading more movies...</p>}
+        scrollThreshold={0.9}
+      >
+        {data.slice(1).map((container, idx) => {
+          if (!container || !container.cd || !Array.isArray(container.cd))
+            return null;
+          return <MoviesList key={idx} movies={container} />;
+        })}
+      </InfiniteScroll>
     </section>
   );
 };
