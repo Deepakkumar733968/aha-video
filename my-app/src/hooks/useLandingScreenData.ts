8a0f66cc6f4c901490f739f7ca3b1f97c6bd0531
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-04-05 18:50:54 +0530

    changes

--- my-app/src/hooks/useLandingScreenData.ts
+++ my-app/src/hooks/useLandingScreenData.ts
@@ -1,33 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { fetchCatalogData } from "../services/api";
 
 export const useLandingScreenData = () => {
   const [data, setData] = useState<any[]>([]);
-  const [page, setPage] = useState(1);
-  const [hasMore, setHasMore] = useState(true);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [hasMore, setHasMore] = useState(true);
 
+  const pageRef = useRef(1);
+  const isFetchingRef = useRef(false);
   const fetchMore = async () => {
+    if (isFetchingRef.current || !hasMore) return;
+
+    isFetchingRef.current = true;
+    setLoading(true);
+
     try {
-      setLoading(true);
-      const result = await fetchCatalogData(page);
-
-      console.log("API Result:", result);
-
+      const result = await fetchCatalogData(pageRef.current);
       const allContainers = result?.t?.map((item: any) => item?.c || []).flat();
 
       if (!allContainers || allContainers.length === 0) {
         setHasMore(false);
       } else {
         setData((prev) => [...prev, ...allContainers]);
-        setPage((prev) => prev + 1);
+        pageRef.current += 1;
       }
     } catch (err) {
-      console.error("Error fetching:", err);
       setError("Failed to fetch data");
+      setHasMore(false);
     } finally {
       setLoading(false);
+      isFetchingRef.current = false;
     }
   };
 
